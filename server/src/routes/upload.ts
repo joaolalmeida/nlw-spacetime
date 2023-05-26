f877a78c9080a3a4141eaa818d83d0fc6fc66ce0
joaolalmeida <joaolalmeida20@gmail.com>
2023-05-26 09:56:34 -0300

    feat:file size limite implemented

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -11,7 +11,7 @@ export async function uploadRoutes(app: FastifyInstance) {
   app.post('/upload', async (request, reply) => {
     const upload = await request.file({
       limits: {
-        fileSize: 5_242_880, // 5mb
+        fileSize: 5_242_880, // 5mb tamanho
       },
     })
 
